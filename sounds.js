565cc90f9dd93b77fb7f2b8f11a6740312ae0972
sophiebierens <103965758+sophiebierens@users.noreply.github.com>
2022-05-18 22:22:22 +0200

    Add files via upload

---
+++ sounds.js
@@ -0,0 +1,18 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+sounds = [
+	0,
+	9,
+	12,
+	15,
+	16.5,
+	24,
+	25.8,
+	31,
+	32,
+	33,
+	75,
+	86,
+	87.5
+	// 87.7
+];
